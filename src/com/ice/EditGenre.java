ec64de50ca5255c8d10f495601525b86e4885ddb
deathline75 <kelvinissebeitechno@hotmail.com>
2016-05-07 17:33:30 +0800

    Fixed EditGenre annoying bug

--- src/com/ice/EditGenre.java
+++ src/com/ice/EditGenre.java
@@ -39,16 +39,13 @@ public class EditGenre extends HttpServlet {
 			response.sendRedirect("login.jsp");
 		else {
 			String genreid = request.getParameter("genreid");
-			System.out.println(genreid);
 			String genrename = request.getParameter("genrename");
 			connectToMysql connection  = new connectToMysql(MyConstants.url);
-			connection.preparedUpdate("update game_genre set genrename=? where genreid=?", genrename,genreid);
-			connection.close();
 			connection.preparedUpdate("update genre set genrename=? where genreid=?", genrename,genreid);
 			connection.close();
 			//response.sendRedirect("genres.jsp");
 			
-	}
+		}
 	}
 
 }
