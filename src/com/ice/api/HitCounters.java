c01b5c51dc9ed427c693b14b17ae61b848fa4a5a
deathline75 <kelvinissebeitechno@hotmail.com>
2016-07-29 13:32:07 +0800

    Improved security by adding sanitization on some pages

--- src/com/ice/api/HitCounters.java
+++ src/com/ice/api/HitCounters.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -15,7 +14,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
 import com.ice.MyConstants;
 import com.ice.connectToMysql;
 
