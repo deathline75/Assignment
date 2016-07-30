f5aeb90254629a3b9b8432fad5d31d84d3e77c0e
pc84560895 <admin@chenqiurong.com>
2016-07-30 22:29:58 +0800

    commit my shit

--- src/com/ice/api/ShopCartItems.java
+++ src/com/ice/api/ShopCartItems.java
@@ -36,53 +36,18 @@ public class ShopCartItems extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// Configure GSON, which is JSON parser from Google
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		// Sets the encoding to UTF-8 because Java...
-		response.setCharacterEncoding("UTF-8");
-		// Starts the connection to MySQL
-		connectToMysql connection = new connectToMysql(MyConstants.url);
-		
-		// Checks if parameters exist
-		if (request.getParameter("userid") != null) {
-			String query = "SELECT * from shop_cart where userid=?";
-			// Gets the data back from the query
-			ResultSet rs = connection.preparedQuery(query, request.getParameter("q-gameid"));
-			
-			try {
-				// Checks if the ResultSet is empty
-				if (rs.isBeforeFirst()) {
-					
-					// Initialize the ArrayList for the GameComments coming in.
-					List<GameComment> gamecomments = new ArrayList<GameComment>();
-					while (rs.next()) {
-						// Add the GameComments into the List
-						gamecomments.add(new GameComment(rs.getInt(2), rs.getInt(1), rs.getString(3), rs.getShort(4),rs.getString(5),rs.getDate(6)));
-					}	
-					// Print out the data along with the response code.
-					response.getWriter().append(gson.toJson(new SearchResult(0, null, gamecomments)));
-				} else {
-					// Prints appropriate response when empty
-					response.getWriter().append(gson.toJson(new SearchResult(-1, "No results found.", null)));
-				}
-			} catch (SQLException e) {
-				// Prints appropriate response when SQLException occurs
-				response.getWriter().append(gson.toJson(new SearchResult(500, e.getMessage(), null)));
-			}
-			
-			connection.close();
-		} else {
-			// Prints appropriate response when there is a lack of data to search for.
-			response.getWriter().append(gson.toJson(new SearchResult(100, "No shop item to search for!", null)));
-		}
+	
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		User user = new User();
+		ShopCartItem shopCartBean = null;
+		connectToMysql connection = new connectToMysql(MyConstants.url);
+		ResultSet rs = connection.preparedQuery("select * from ",user.getId());
+		
 	}
 
 }
